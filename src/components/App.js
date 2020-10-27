671d49034cb9629b22156eee8843c29ad40aaa7e
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-10-27 13:52:17 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,53 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+    setBallPosition({
+      left: "0px",
+      top: "0px"
+    });
+  };
+  const startGame = () => {
+    setRenderBall(true);
+  };
+
+  const move = (el) => {
+    const ballPositionCopy = { ...ballPosition };
+    if (el.key === "ArrowRight")
+      ballPositionCopy.left = +ballPositionCopy.left.slice(0, -2) + 5 + "px";
+    else if (el.key === "ArrowLeft")
+      ballPositionCopy.left = +ballPositionCopy.left.slice(0, -2) - 5 + "px";
+    else if (el.key === "ArrowUp")
+      ballPositionCopy.top = +ballPositionCopy.top.slice(0, -2) - 5 + "px";
+    else if (el.key === "ArrowDown")
+      ballPositionCopy.top = +ballPositionCopy.top.slice(0, -2) + 5 + "px";
+    setBallPosition(ballPositionCopy);
+  };
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", move);
+
+    return () => {
+      document.removeEventListener("keydown", move);
+    };
+  });
+
+  const renderChoice = () => {
+    if (renderBall) return <div className="ball" style={ballPosition}></div>;
+    else
+      return (
+        <button onClick={startGame} className="reset">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
